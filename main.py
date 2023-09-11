f937414572820e80668f994bba99695a91384a58
Oreilly Lab <lab@oreilly.com>
2023-09-11 17:33:30 +0000

    added main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+import json
+import jsonschema
+
+def validate_json(json_data, schema_data):
+    try:
+        # loading JSON Schema
+        schema = json.load(schema_data)
+
+        # loading JSON doc
+        doc = json.load(json_data)
+
+        # creating validator
+        validator = jsonschema.Draft7Validator(schema)
+
+        # validating doc against schema
+        errors = list(validator.iter_errors(doc))
+
+        if not errors:
+            print('Doc is Valid according to the schema')
+        else:
+            print('JSON doc is not valid. Errors: ')
+            for err in errors:
+                print(err.message)
+
+    except json.JSONDecodeError as e:
+        print('Error Decoding JSON: ', e)
+    except Exception as e:
+        print('Error: ', e)
+
+if __name__ == '__main__':
+    json_doc_path = ''
+    json_schema_path = ''
+
+    # reading both files
+    with open(json_doc_path, 'r') as json_file, open(json_schema_path, 'r') as schema_file:
+        json_data = json_file.read()
+        schema_data = schema_file.read()
+
+    # function call
+    validate_json(json_data, schema_data)
